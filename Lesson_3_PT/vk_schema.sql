28137a34e477a8ac58f80cfef31b1944dd9abb50
Andrey T <wematall@gmail.com>
2020-06-15 10:02:33 +0300

    create db vk. Table users added

---
+++ Lesson_3_PT/vk_schema.sql
@@ -0,0 +1,17 @@
+-- Предварительно создать БД vk
+-- CREATE DATABASE vk;
+-- переключиться на использование новой бд
+-- USE vk;
+
+-- Создаём таблицу пользователей
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", 
+  first_name VARCHAR(100) NOT NULL COMMENT "Имя пользователя",
+  last_name VARCHAR(100) NOT NULL COMMENT "Фамилия пользователя",
+  email VARCHAR(100) NOT NULL UNIQUE COMMENT "Почта",
+  phone VARCHAR(100) NOT NULL UNIQUE COMMENT "Телефон",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Пользователи";  
+

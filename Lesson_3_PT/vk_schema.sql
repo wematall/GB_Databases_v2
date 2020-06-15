dfe57d51af4b9f326921ce8194af23efe979df7e
Andrey T <wematall@gmail.com>
2020-06-15 10:03:34 +0300

    Table profiles added

--- Lesson_3_PT/vk_schema.sql
+++ Lesson_3_PT/vk_schema.sql
@@ -15,3 +15,15 @@ CREATE TABLE users (
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
 ) COMMENT "Пользователи";  
 
+-- Таблица профилей
+DROP TABLE IF EXISTS profiles;
+CREATE TABLE profiles (
+  user_id INT UNSIGNED NOT NULL PRIMARY KEY COMMENT "Ссылка на пользователя", 
+  gender CHAR(1) NOT NULL COMMENT "Пол",
+  birthday DATE COMMENT "Дата рождения",
+  city VARCHAR(130) COMMENT "Город проживания",
+  country VARCHAR(130) COMMENT "Страна проживания",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Профили"; 
+

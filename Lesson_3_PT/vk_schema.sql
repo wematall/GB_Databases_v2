1547b1e3af9e83c8c695f24d88f1a8e92c6c1059
Andrey T <wematall@gmail.com>
2020-06-15 10:08:59 +0300

    Table media added

--- Lesson_3_PT/vk_schema.sql
+++ Lesson_3_PT/vk_schema.sql
@@ -79,4 +79,17 @@ CREATE TABLE communities_users (
   PRIMARY KEY (community_id, user_id) COMMENT "Составной первичный ключ"
 ) COMMENT "Участники групп, связь между пользователями и группами";
 
+-- Таблица медиафайлов
+DROP TABLE IF EXISTS media;
+CREATE TABLE media (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки",
+  user_id INT UNSIGNED NOT NULL COMMENT "Ссылка на пользователя, который загрузил файл",
+  filename VARCHAR(255) NOT NULL COMMENT "Путь к файлу",
+  size INT NOT NULL COMMENT "Размер файла",
+  metadata JSON COMMENT "Метаданные файла",
+  media_type_id INT UNSIGNED NOT NULL COMMENT "Ссылка на тип контента",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Медиафайлы";
+
 
